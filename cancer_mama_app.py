e81dac0d4217d9731ff95792218405f571abb41e
Tomi <tomi.ambro94@gmail.com>
2020-10-22 00:58:41 -0300

    Agrego primeras partes del script

--- cancer_mama_app.py
+++ cancer_mama_app.py
@@ -17,6 +17,13 @@ def load_data():
 	df.drop(['id', 'Unnamed: 32'], axis=1, inplace=True)
 	return df
 
+@st.cache
+def get_pairplot(data):
+	fig, ax = plt.subplots(figsize=(20,8))
+	sns.pairplot(data=data, hue='diagnosis', palette='Set2', height=1.5)
+	# plt.show()
+	return fig
+
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 data = load_data()
@@ -24,12 +31,21 @@ data = load_data()
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
 
-st.subheader('Columns')
-st.write(data.columns)
-
 st.subheader('Raw data')
 st.write(data.head())
 
-fig, ax = plt.subplots()
-sns.boxplot(data=data)
+st.subheader('Boxplots')
+fig, ax = plt.subplots(figsize=(20,8))
+# fig.figure(figsize=(16,5))
+sns.boxplot(data=data.iloc[:, :14])
 st.pyplot(fig)
+
+fig, ax = plt.subplots(figsize=(20,8))
+sns.boxplot(data=data.iloc[:, 14:])
+st.pyplot(fig)
+
+data_load_state = st.text('Loading data...')
+# st.subheader('Pairplot')
+# plot = get_pairplot(data.iloc[:,:3])
+# st.pyplot(fig)
+data_load_state.text('Loading data...done!')
